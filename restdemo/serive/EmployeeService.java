e2c49dafd590965197c85912037d4b18bcdae213
kkedarThinkitive <kedar.ganmote@thinkitive.com>
2024-01-16 20:43:01 +0530

    Created SpringBoot CRUD Application

---
+++ restdemo/serive/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.crud.restdemo.serive;
+
+
+
+import com.crud.restdemo.dao.EmployeeEntity;
+
+
+import java.util.List;
+
+
+public interface EmployeeService {
+
+
+    public List<EmployeeEntity> getAllEmployee();
+    public EmployeeEntity getEmployeeByID(int id);
+    public String createEmployee(EmployeeEntity employee);
+    public String updateEmployee(EmployeeEntity employee);
+    public String deleteEmployee(int id);
+
+
+
+}
